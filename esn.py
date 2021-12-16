0eb3e61bdb68dcd999bf0ee12a03706f2c7680ca
Sakib Rahman <rahmans@beluga1.int.ets1.calculquebec.ca>
2021-12-15 19:52:17 -0500

    Initialize class for timeseries datasets

--- esn.py
+++ esn.py
@@ -11,3 +11,16 @@ from scipy.spatial.distance import cdist
 from scipy.linalg import solve
 # make sure plots are displayed correctly
 %matplotlib inline
+
+
+"""Class for dataset"""
+
+class io:
+    def __init__(self, csv_path):
+        df = pd.read_csv(csv_path, header=None)
+        input = df[0].to_numpy()
+        self.mean=np.mean(input)
+        self.std=np.std(input)
+        self.input= (input-self.mean)/self.std
+        return
+
