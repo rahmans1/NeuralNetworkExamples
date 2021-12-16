556026bf22217705de6fe4e67176cd954f665a9b
Sakib Rahman <rahmans@beluga1.int.ets1.calculquebec.ca>
2021-12-15 19:16:31 -0500

    Add pre-requisites

---
+++ esn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from scipy import sparse
+from scipy import stats
+from scipy import signal
+from scipy.spatial.distance import cdist
+from scipy.linalg import solve
+# make sure plots are displayed correctly
+%matplotlib inline
