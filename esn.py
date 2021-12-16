e9524ac7df2063f1d3f7fec468ee2fb625034332
Sakib Rahman <rahmans@beluga1.int.ets1.calculquebec.ca>
2021-12-15 20:22:26 -0500

    Calculate autocorrelation and automutual information to calculate correct embedding dimension m

--- esn.py
+++ esn.py
@@ -24,3 +24,22 @@ class io:
         self.input= (input-self.mean)/self.std
         return
 
+    """Calculate autocorrelation of data"""
+    @property
+    def acf(self):
+        acf = np.correlate(self.input, self.input, mode='full')
+        return acf[int(np.floor(acf.size/2)):]
+
+    """Calculate automutual information of data"""
+    @property
+    def amf(self,bins=20, max_tau=200):
+        amf=[]
+        p_i, _ = np.histogram(self.input, bins, density=True)
+        sum_p_i_log_p_i = np.nansum(p_i*np.log(p_i))
+        for tau in range(0,max_tau):
+          x = self.input[:self.input.size-tau]
+          y = self.input[tau:]
+          p_ij, _, _ = np.histogram2d(x, y, bins=[bins,bins], density=True)
+          sum_p_ij_log_p_ij= np.nansum(p_ij*np.log(p_ij))
+          amf.append(sum_p_ij_log_p_ij-sum_p_i_log_p_i)
+        return np.array(amf)
