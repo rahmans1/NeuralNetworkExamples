bac1afb0f1922e0e94584746651f3dcfc9096f23
Sakib Rahman <rahmans@beluga1.int.ets1.calculquebec.ca>
2021-12-15 20:30:04 -0500

    Calculate timelag from the acf and amf and use it to calculate m based on false nearest neighbor ratio

--- esn.py
+++ esn.py
@@ -43,3 +43,42 @@ class io:
           sum_p_ij_log_p_ij= np.nansum(p_ij*np.log(p_ij))
           amf.append(sum_p_ij_log_p_ij-sum_p_i_log_p_i)
         return np.array(amf)
+
+     """The time lag is taken to be the minimum of the first zero of acf and the first minimum of amf"""
+    @property
+    def tau(self):
+        acf_tau = np.argwhere(self.acf<0)[0]-1
+        amf_tau= np.array(signal.argrelextrema(self.amf,np.less))
+        tau= np.array([acf_tau, amf_tau[0][0]])
+        min_tau= np.min(tau.all())
+        return min_tau
+
+    """Calculate the False Nearest Neighbor Ratio for a default heuristic r upto maximum embedding dimension m"""
+    def compute_m(self, r=5, max_m=10):
+        s={}
+        pairwise={}
+        for m in range(1,max_m+2):
+            s[m]=[]
+            s[m].append(self.input)
+            for i in range(m-1, 0,-1):
+                s[m].append(np.roll(self.input, (m-i)*self.tau))
+            s[m]=np.transpose(s[m])
+            distance=cdist(s[m], s[m], metric='chebyshev')
+            np.fill_diagonal(distance, np.nan)
+            pairwise[m]=(distance[~np.isnan(distance)].reshape(distance.shape[0], distance.shape[1] - 1)).min(axis=1)
+
+        sigma= np.std(self.input)
+        for m in range(1, max_m+1):
+            metric1=pairwise[m+1]/pairwise[m]-r
+            metric1[metric1<0]=0
+            metric1[metric1>0]=1
+            metric2= sigma/r-pairwise[m]
+            metric2[metric2<0]=0
+            metric2[metric2>0]=1
+            fnn_ratio=np.sum(metric1*metric2)/np.sum(metric2)
+            plt.scatter(m, fnn_ratio)
+        plt.yscale('log')
+        plt.title("False Nearest Neighbor Ratio vs m (r="+str(r)+")")
+        plt.show()
+        return
+
